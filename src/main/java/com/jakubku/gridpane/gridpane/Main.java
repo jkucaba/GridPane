5e3baf1757a0829e160e2fc82d63ffedcc0ab1d3
Jakub <jakub.kucaba@gmail.com>
2023-10-20 10:37:22 +0200

    Another Ex

--- src/main/java/com/jakubku/gridpane/gridpane/Main.java
+++ src/main/java/com/jakubku/gridpane/gridpane/Main.java
@@ -3,6 +3,9 @@ package com.jakubku.gridpane.gridpane;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -10,18 +13,22 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         GridPane root = new GridPane();
-        root.setGridLinesVisible(true);
-        Button b1 = new Button("One");
-        Button b2 = new Button("Two");
-        Button b3 = new Button("Three");
+        //root.setGridLinesVisible(true);
+        root.setHgap(10);
+        root.setVgap(10);
 
-        root.getChildren().addAll(b1,b2,b3);
+        Label label = new Label("First Name");
+        TextField field = new TextField();
+        Button button = new Button("COMMENT");
+        TextArea area = new TextArea();
+        area.setPromptText("Enter Your Message Here");
 
-        GridPane.setConstraints(b1, 0, 0);
-        GridPane.setConstraints(b2, 1, 0);
-        GridPane.setConstraints(b3, 0, 1);
+        root.addRow(0, label, field, button);
+        root.add(area, 0, 1, GridPane.REMAINING, 1);
+
         root.setStyle("-fx-padding: 10;"+"-fx-border-style: solid inside;"+"-fx-border-width: 2;"+"-fx-border-insets: 5;"+ "-fx-border-radius: 5;"+"-fx-border-color: blue;");
         Scene scene = new Scene(root);
+        root.requestFocus(); //nothing highlighted
         stage.setTitle("GridPane Layout Example");
         stage.setScene(scene);
         stage.show();
